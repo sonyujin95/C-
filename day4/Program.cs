571a72d3fe97050ecd3fb23be976f7186a23d261
sonyujin95 <84109579+sonyujin95@users.noreply.github.com>
2022-03-03 11:09:20 +0900

    에러코드 입력받아서 에러메세지 리턴하기

---
+++ day4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int errorCode = 130;
+
+            string s = ErrorDescription(errorCode);
+
+            Console.WriteLine(s);
+        }
+
+        static string ErrorDescription(int errorCode)
+        {
+            string desc;
+
+            switch (errorCode)
+            {
+                case 1100:
+                    desc = "입력 데이터가 없습니다.";
+                    break;
+
+                case 1200:
+                    desc = "잘못된 입력값입니다.";
+                    break;
+
+                case 1300:
+                    desc = "입력 범위를 초과하였습니다.";
+                    break;
+                default:
+                    desc = "Unknown Error";
+                    break;
+            }
+
+            return desc;
+        } 
+    }
+}
