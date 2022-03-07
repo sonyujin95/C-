0eb7e9c09966085a9cf7e106ef606c8055712031
sonyujin95 <84109579+sonyujin95@users.noreply.github.com>
2022-03-07 10:33:58 +0900

    foreach, do...while문 실습

---
+++ day4/day4_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace day4_2
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            // 배열을 foreach 를 사용하여 출력
+            string[] s = { "Jim", "Sam", "Kim", "Park"};
+
+            foreach (string x in s)
+            {
+                Console.WriteLine(x);
+            }
+
+            // while문으로 1부터 100까지 합계 구하기
+            int sum = 0;
+            int i = 1;
+
+            while (i <= 100)
+            {
+                sum = sum + i;
+                i++;
+            }
+            Console.WriteLine(sum);
+
+            // do..while 을 사용한 반복문
+            // 아래는 while 문이 거짓이 되지만
+            // Hello를 한번은 출력함
+            int j = 0;
+            do
+            {
+                Console.WriteLine("Hello");
+                j++;
+            } while (j<0);
+        }
+    }
+}
