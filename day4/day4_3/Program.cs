c59fb82a5114c92c0e1741e2c8f2b64dba0973b1
sonyujin95 <84109579+sonyujin95@users.noreply.github.com>
2022-03-11 10:39:32 +0900

    Add files via upload

---
+++ day4/day4_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace day4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 0;
+
+            try
+            {
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
