0dd736051b27485e478bd94c3d8ed73419b13c84
Yujin Son <84109579+sonyujin95@users.noreply.github.com>
2022-05-26 08:27:14 +0900

    variables

---
+++ Variables/Variable_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double A = 10.5;
+
+            // placeholder 방식이 아닌 interpolation 방식을 사용하고 있다.
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"A = {A}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string name = "손유진";
+            char exmark = '!';
+            //sbyte age;
+            //age = 26;
+            sbyte age = 26;  // 두 가지 방식 가능
+
+            Console.WriteLine("Hello " + name + exmark);
+            Console.Write("You are " + age + " years old" + exmark);
+        }
+    }
+}
